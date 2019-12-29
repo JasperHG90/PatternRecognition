fb8fedca65674a8e648836295ad698bc2e88e8b8
Jasper Ginn <jasperginn@gmail.com>
2019-12-29 12:35:50 +0100

    Switch to Skorch for kfold. Minor updates on preprocessing and modeling

--- 2_baseline_model.py
+++ 2_baseline_model.py
@@ -22,7 +22,7 @@ args = Namespace(
   # File to save results
   out_file = 'results/basicNN_trials.csv',
   # Number of times to evaluate bayesian search for hyperparams
-  max_evals = 800,
+  max_evals = 500,
   # Size of the vocabulary
   input_vocabulary_size = 15000,
   # Embedding size
@@ -65,11 +65,6 @@ for idx, itms in enumerate(input_data.items()):
       xo += 1
     train_x.append(" ".join(par_out).replace("'s", ""))
     train_y.append(cat)
-
-# Preprocess outcome label
-train_y_ohe = np.zeros((len(train_y), len(input_data.keys())))
-for idx,lbl in enumerate(train_y):
-  train_y_ohe[idx, lbl] = 1
 
 # Create tokenizer
 tokenizer = Tokenizer(num_words=args.input_vocabulary_size,
@@ -114,8 +109,9 @@ with open("embeddings/prep.pickle", "rb") as inFile:
 # Set up a softmax layer
 # One-layer NN with softmax on top
 class BaselineNN(nn.Module):
-    def __init__(self, weights, num_classes, hidden_dim, p_dropout = 0):
+    def __init__(self, weights, num_classes, hidden_dim, p_dropout = 0, use_batch_norm = True):
         super(BaselineNN, self).__init__()
+        # Set dropout percentage
         self._p_dropout = p_dropout
         # Get embedding dimensions
         self.weights_dim = weights.shape[1]
@@ -123,41 +119,47 @@ class BaselineNN(nn.Module):
         self.embedding = Embedding_FastText(weights, freeze_layer = True)
         # Set up hidden layer
         self.linear1 = nn.Linear(self.weights_dim, hidden_dim)
+        # Set up batch norm
+        self.bn = nn.BatchNorm1d(num_features=hidden_dim)
+        self.use_batch_norm = use_batch_norm
         # Set up softmax layer
         self.linear2 = nn.Linear(hidden_dim, num_classes)
     def forward(self, input, dropout = 0):
         # Call embedding
         embedded = self.embedding(input).sum(dim=1)
         # Predict
-        yhat = F.relu(self.linear1(embedded))
+        yhat = self.linear1(embedded)
+        # Apply batch norm
+        if self.use_batch_norm:
+          yhat = self.bn(yhat)
+        # Activation
+        yhat = F.relu(yhat)
         # Dropout
         yhat = F.dropout(yhat, p = self._p_dropout)
         # Linear
         yhat = self.linear2(yhat)
-        # Probabilities (logged)
+        # Probabilities
         yhat = F.softmax(yhat, dim=1)
         return(yhat)
 
 # Load data in Pytorch 'Dataset' format
 # See 'model_utils.py'
-VitalArticles = WikiData(train, train_y_ohe)
+VitalArticles = WikiData(train, np.array(train_y))
 
 # Split data
 # Returns two instances of 'WikiData' (train and test)
 trainx, test = split(VitalArticles, val_prop = .05)
 
 # Class weights
+# Preprocess outcome label 
+train_y_ohe = np.zeros((len(train_y), len(input_data.keys())))
+for idx,lbl in enumerate(train_y):
+  train_y_ohe[idx, lbl] = 1
 # These weights are unnormalized but that's what pytorch is expecting
 cw = torch.tensor(np.max(np.sum(train_y_ohe, axis=0)) / (np.sum(train_y_ohe, axis=0))).type(torch.float).to(device)
-# Set up the classifier
-# Hidden dim neurons: 128
-#mlr = BaselineNN(FTEMB, train_y_ohe.shape[1], 128)
-#mlr = mlr.to(device)  
-#loss_function = nn.CrossEntropyLoss(weight=cw)  
-# Optimizer
-#optimizer = optim.Adam(mlr.parameters(), lr=args.learning_rate)
 
 ### Use hyperopt (Bayesian hyperparameter optimization) to search for good hyperparams
+
 from hyperopt import STATUS_OK
 import csv
 from hyperopt import hp
@@ -167,40 +169,88 @@ from hyperopt import tpe
 from hyperopt import Trials
 # Optimizer criterion
 from hyperopt import fmin
+# Use skorch for cross-validation
+from skorch import NeuralNet
+from skorch.dataset import CVSplit
+# Tracking precision//recall//F1
+from sklearn import metrics
 
 # Function that sets up model and outputs and returns validation loss
 def baselineNN_search(parameters):
   """Set up, run and evaluate a baseline neural network"""
-  mlr = BaselineNN(FTEMB, train_y_ohe.shape[1], parameters["hidden_units"], p_dropout = parameters["dropout"])
-  # To device
-  mlr = mlr.to(device)  
-  loss_function = nn.CrossEntropyLoss(weight=cw)  
-  # Optimizer
-  if parameters["optimizer"] == "Adam":
-    optimizer = optim.Adam(mlr.parameters(), lr=parameters["learning_rate"])
-  else:
-    optimizer = optim.RMSprop(mlr.parameters(), lr=parameters["learning_rate"])
-  # Train using CV
-  _, io = train_model(mlr, trainx, optimizer, loss_function, 25, 0.1, 128, device = device)
-  # Write
-  is_min = np.argmin(io["val_loss"])
+  # Split into train/test set
+  train_current, test_current = split(trainx, val_prop = .05) 
+  # CV with skorch
+  net = NeuralNet(
+    # Module
+    module=BaselineNN,
+    # Module settings
+    module__hidden_dim = parameters["hidden_units"],
+    module__p_dropout = parameters["dropout"],
+    module__use_batch_norm = parameters["use_batch_norm"],
+    module__weights = FTEMB, # These are word embeddings
+    module__num_classes = len(catmap),
+    # Epochs & learning rate
+    max_epochs=25,
+    lr=parameters["learning_rate"],
+    # Optimizer
+    optimizer=optim.Adam if parameters["optimizer"] == "Adam" else optim.RMSprop,
+    # Loss function
+    criterion=nn.CrossEntropyLoss,
+    # Shuffle training data on each epoch
+    iterator_train__shuffle=True,
+    # Batch size
+    batch_size = 128,
+    train_split = CVSplit(cv=5),
+    # Device
+    device = device
+  )
+  # Add class weights
+  if parameters["weighted_loss"]:
+    net.criterion__weight = cw
+  # Verbose to false
+  net.verbose = 0
+  # Fit
+  net = net.fit(train_current)
+  # Get train / validation history
+  train_loss = net.history[:,"train_loss"]
+  val_loss = net.history[:, "valid_loss"]
+  # Min loss
+  which_min = np.argmin(val_loss)
+  # Predict on hold-out set
+  yhat = net.predict(test_current)
+  yhatc = yhat.argmax(axis=1)
+  ytrue = test_current.y
+  # Get accuracy
+  acc_test = np.round((ytrue == yhatc).sum() / yhatc.size, 4)
+  # Prec/rec/f1
+  out_metrics = metrics.precision_recall_fscore_support(ytrue, 
+                                                        yhatc, 
+                                                        average = "weighted")
+  # Write to file
   with open(args.out_file, 'a') as of_connection:
     writer = csv.writer(of_connection)
-    writer.writerow([io["val_loss"][is_min], parameters, is_min, io["val_acc"][is_min]])
+    writer.writerow([parameters, np.round(train_loss[which_min], 4), 
+                     np.round(val_loss[which_min], 4), which_min, 
+                     np.round(acc_test, 4), np.round(out_metrics[0], 4), 
+                     np.round(out_metrics[1], 4), np.round(out_metrics[2], 4)])
   # Return cross-validation loss
-  return({"loss": np.min(io["val_loss"]), "parameters": parameters, "iteration": is_min, 'status':STATUS_OK})
-
-# Test if works  
-parameters = {"learning_rate": 0.00104, "hidden_units": 256, "dropout": 0.016, "optimizer": "Adam"}
-po = baselineNN_search(parameters)
+  return({"loss": val_loss[which_min], "parameters": parameters, "iteration": which_min, 'status':STATUS_OK})
 
 # Define the search space
 space = {
-    'hidden_units': hp.choice('hidden_units', [64,128,256]),
-    'optimizer': hp.choice("optimizer", ["Adam"]),
-    'dropout': hp.uniform("dropout", 0, 0.5),
-    'learning_rate': hp.loguniform('learning_rate', np.log(0.0001), np.log(0.01))
+    'hidden_units': hp.choice('hidden_units', [64,128,256,512]),
+    'optimizer': hp.choice("optimizer", ["Adam", "RMSprop"]),
+    'weighted_loss': hp.choice("weighted_loss", [True, False]),
+    'use_batch_norm': hp.choice("use_batch_norm", [True, False]),
+    'dropout': hp.choice("dropout", [0, .05, .1, .15, .2, .25, .3, .35, .4, .45, .5]),
+    'learning_rate': hp.loguniform('learning_rate', np.log(0.001), np.log(0.01))
 }
+
+# Test if works  
+from hyperopt.pyll.stochastic import sample
+parameters = sample(space)
+po = baselineNN_search(parameters)
 
 # Algorithm
 tpe_algorithm = tpe.suggest
@@ -211,42 +261,62 @@ bayes_trials = Trials()
 with open(args.out_file, 'w') as of_connection:
   writer = csv.writer(of_connection)
   # Write the headers to the file
-  writer.writerow(['loss', 'params', 'iteration', 'accuracy'])
+  writer.writerow(['params', 'train_loss', 'val_loss', 'iteration', 'test_accuracy', "test_f1", "test_precision", "test_recall"])
 
 # Optimize
 best = fmin(fn = baselineNN_search, space = space, algo = tpe.suggest, 
             max_evals = args.max_evals, trials = bayes_trials)
             
 # Save
-import json
-with open("hyperparameters/best_baseline.json", "w") as outFile:
-  # Cast to serializable files
-  best["dropout"] = float(best["dropout"])
-  best["hidden_units"] = 256
-  best["learning_rate"] = float(best["learning_rate"])
+with open("hyperparameters/best_baseline.pickle", "wb") as outFile:
   best["optimizer"] = "Adam"
-  json.dump(best, outFile)
+  best["use_batch_norm"] = True
+  best["hidden_units"] = 256
+  pickle.dump(best, outFile)
   
 # Load
-with open("hyperparameters/best_baseline.json", "r") as inFile:
-  parameters = json.load(inFile)
+with open("hyperparameters/best_baseline.pickle", "rb") as inFile:
+  parameters = pickle.load(inFile)
+  
+## Run on final test data
 
-# To device
-mlr = BaselineNN(FTEMB, train_y_ohe.shape[1], parameters["hidden_units"], p_dropout = parameters["dropout"])
-mlr = mlr.to(device)  
-loss_function = nn.CrossEntropyLoss(weight=cw)  
-# Optimizer
-optimizer = optim.Adam(mlr.parameters(), lr=parameters["learning_rate"])
-mlr, io = train_model(mlr, trainx, optimizer, loss_function, 25, 0.1, 128, device = device)
+# Run the model with the best paramaters
+net = NeuralNet(
+    # Module
+    module=BaselineNN,
+    # Module settings
+    module__hidden_dim = 256,
+    module__p_dropout = 0.1,
+    module__use_batch_norm = True,
+    module__weights = FTEMB,
+    module__num_classes = len(catmap),
+    # Epochs & learning rate
+    max_epochs=25,
+    lr=0.00214,
+    # Optimizer
+    optimizer=optim.Adam,
+    # Loss function
+    criterion=nn.CrossEntropyLoss,
+    criterion__weight = cw,
+    # Shuffle training data on each epoch
+    iterator_train__shuffle=True,
+    # Batch size
+    batch_size = 128,
+    train_split = CVSplit(cv=5),
+    # Device
+    device = device
+)
 
-mlr.eval()
-# Predict
-y_pred = mlr(torch.tensor(test.X).type(torch.long).to(device))
-# Retrieve true y
-val, y_true = torch.tensor(test.y).type(torch.long).to(device).max(dim=1)
-# Loss
-loss_val = loss_function(y_pred, y_true)
-# Accuracy
-_, y_pred = y_pred.max(dim=1)
-acc_val = np.int((y_pred == y_true).sum()) / y_pred.size()[0]
-acc_val
+# Verbose to false
+net.verbose = 0
+# Fit
+io = net.fit(trainx)
+yhat = net.predict(test)
+yhatc = yhat.argmax(axis=1)
+ytrue = test.y
+(ytrue == yhatc).sum() / yhatc.size
+  
+# Classification report
+from sklearn import metrics
+print(metrics.classification_report(ytrue, yhatc, target_names=list(catmap.values())))
+metrics.confusion_matrix(ytrue, yhatc)
