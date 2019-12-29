3c38cc84c8c011e89d15bcef5f9b6ca54a40c4f2
Jasper Ginn <jasperginn@gmail.com>
2019-12-29 15:43:12 +0100

    Small changes

--- evaluate_results.R
+++ evaluate_results.R
@@ -29,10 +29,13 @@ outpar <- function(x) {
 library(purrr)
 library(dplyr)
 hparams_processed <- map_df(hparams, outpar) %>%
-  mutate(loss = io$loss) %>%
+  mutate(loss = io$val_loss,
+         test_acc = io$test_accuracy,
+         test_f1 = io$test_f1) %>%
   mutate(use_batch_norm = ifelse(use_batch_norm == "True", TRUE, FALSE),
          quantilegrp = cut(loss, quantile(loss)),
-         optimizer_adam = ifelse(optimizer == "Adam", TRUE, FALSE)) %>%
+         optimizer_adam = ifelse(optimizer == "Adam", TRUE, FALSE),
+         weighted_loss = ifelse(weighted_loss == "True", TRUE, FALSE)) %>%
   select(-optimizer)
 
 # Plot some density plots
@@ -64,8 +67,13 @@ ggplot(hparams_processed, aes(x=(learning_rate))) +
 # Mean learning rate
 exp(mean(log(hparams_processed$learning_rate)))
 
+# Across optimizers
+ggplot(hparams_processed %>%
+         mutate(iteration = 1:n()), aes(x=iteration, y=learning_rate, color=optimizer_adam)) +
+         geom_point()
+
 # Learning rate versus dropout
-p2 <- ggplot(hparams_processed, aes(x = log(learning_rate), y = log(dropout))) +
+p2 <- ggplot(hparams_processed, aes(x = log(learning_rate), y = dropout)) +
   geom_point() +
   geom_vline(aes(xintercept = hparams_processed %>%
                    filter(loss == min(loss)) %>%
@@ -77,8 +85,7 @@ p2 <- ggplot(hparams_processed, aes(x = log(learning_rate), y = log(dropout))) +
   geom_hline(aes(yintercept = hparams_processed %>%
                    filter(loss == min(loss)) %>%
                    select(dropout) %>%
-                   pull() %>%
-                   log(.)),
+                   pull() ),
              color = "green", linetype = "dashed", 
              size = 1.2)
 ggExtra::ggMarginal(p2, type = "histogram")
@@ -93,12 +100,12 @@ ggplot(hparams_processed %>% group_by(filter_size_1) %>%
          tally(), aes(x= filter_size_1, y = n)) +
   geom_bar(stat = "identity")
 
-ggplot(hparams_processed, aes(x=learning_rate, y = loss)) +
+ggplot(hparams_processed, aes(x=learning_rate, y = loss, color=optimizer_adam)) +
   geom_point() +
   geom_smooth()
 # Anything beyond 0.002 is a waste of time basically.
 
-ggplot(hparams_processed, aes(x=dropout, y = loss)) +
+ggplot(hparams_processed, aes(x=dropout, y = loss, shape = optimizer_adam)) +
   geom_point() +
   geom_smooth()
 # Less clear. Trend seems to be: more dropout: lower test set performance
